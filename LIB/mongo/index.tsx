ef4f412d22f08ba2e15eb0260e98e91273109972
choki-dorji <ceedeejee9@gmail.com>
2023-08-23 13:57:41 +0600

    mongo

--- LIB/mongo/index.tsx
+++ LIB/mongo/index.tsx
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const connectMongooDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(
+      process.env.MONGO_URI ??
+        "mongodb+srv://Bumthap:Bumthap123@cluster0.7i7nwco.mongodb.net/"
+    );
     console.log("Connected to Mongo");
   } catch (e) {
     console.log(e);
