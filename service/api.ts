f68d11b49759b10d13144568e2a6bd4d2ac4151b
choki dorji <choki.dorji@selise.ch>
2023-12-21 11:28:07 +0600

    refractor

---
+++ service/api.ts
@@ -0,0 +1,11 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BpData, Bmidata, BMIData, bloodData } from "@/types";
+
+const api = createApi({
+  reducerPath: "BloodSlice",
+  baseQuery: fetchBaseQuery({ baseUrl:  process.env.NEXT_PUBLIC_NEXT_DOMAIN }),
+  tagTypes: ['bp', "weight"],
+  endpoints: (builder) => ({ }),
+});
+
+export default api;
